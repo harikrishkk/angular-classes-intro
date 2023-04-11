a150c5d5bb094dc5027bd86a4c8ec4669c685954
Hari <harikrishkk@gmail.com>
2023-04-10 23:23:44 -0400

    commit 31: add validations

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,10 +9,26 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
 
+  get userEmail() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  get isEmailValid() {
+    return !this.loginForm.get('email')?.valid && this.loginForm.get('email')?.touched;
+  }
+
+  get isPasswordValid() {
+    return !this.loginForm.get('password')?.valid && this.loginForm.get('password')?.touched;
+  }
+
   ngOnInit() {
     this.loginForm = new FormGroup({
-      'email': new FormControl(null),
-      'password': new FormControl(null)
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required, Validators.minLength(6)])
     })
   }
   onSubmit() {
