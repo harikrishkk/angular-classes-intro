14ac2e8e7530d2a7cfdd84f3c1f5765721d25468
Hari <harikrishkk@gmail.com>
2023-04-11 08:58:20 -0400

    commit 35: add shoping cart page

--- src/app/shopping-cart/cart/cart.component.ts
+++ src/app/shopping-cart/cart/cart.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { faTrash, faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
-
+  faTrash = faTrash;
+  faMinus = faMinus;
+  faPlus = faPlus;
 }
--- src/app/shopping-cart/shopping-cart.module.ts
+++ src/app/shopping-cart/shopping-cart.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShoppingCartRoutingModule } from './shopping-cart.routing.module';
 import { CartComponent } from './cart/cart.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [CartComponent],
   imports: [
     CommonModule,
-    ShoppingCartRoutingModule
+    ShoppingCartRoutingModule,
+    FontAwesomeModule
   ]
 })
 export class ShoppingCartModule { }
